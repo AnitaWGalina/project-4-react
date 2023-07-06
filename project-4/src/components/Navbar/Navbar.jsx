0d794837395ea7df5891f4870311e1e9e1008a57
Cassie <cassierasugu@gmail.com>
2023-07-06 08:29:50 +0300

    images

--- project-4/src/components/Navbar/Navbar.jsx
+++ project-4/src/components/Navbar/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import "./Navbar.css";
-import logoImg from "../../images/logo.jpg";
+import logoImg from "../../images/logo0.jpg";
 import { HiOutlineMenuAlt3 } from "react-icons/hi";
 
 const Navbar = () => {
