4e05b6f42666ede2087c0ca3c8c5edb14a82f31f
anitagalina <anitagalina21@gmail.com>
2023-07-06 10:27:19 +0300

    commit changes

--- project-4/src/App.js
+++ project-4/src/App.js
@@ -1,5 +1,9 @@
 
-import './App.css';
+// import './App.css';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import RegistrationForm from "./Registration";
 import Login from "./Login";
 // import { ChakraProvider } from '@chakra-ui/react'
@@ -9,13 +13,15 @@ import Header from './components/Header/Header';
 import Home from './pages/Home/Home'
 import Login from './Login';
 import About from "./pages/About/About";
+import "./index.css";
 
 
 function App() {
-  return (
-    // <ChakraProvider>
+  const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+   <ChakraProvider>
+  <BrowserRouter>
     <div className="App">
-    
       <Router>
         <Routes>
           <Route exact path="/" element={<RegistrationForm />} />
@@ -25,13 +31,13 @@ function App() {
           <Route exact path="/header" element={<Header />} />
           <Route path="/" element={<Home />} />
           <Route exact path="/registration" element={<RegistrationForm />} />
-          <Route path="about" element={<About />} />
+          <Route path="/about" element={<About />} />
         </Routes>
       </Router>
     </div>
-    // </ChakraProvider>
-  
-  );
+  </BrowserRouter>
+  // </ChakraProvider>
+);
 }
 
 
